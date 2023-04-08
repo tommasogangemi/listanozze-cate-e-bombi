df2c2b041e8d27edbd24a600a7c6b883fabd0a3f
tommasogangemi <tommaso.gangemi@kampaay.com>
2023-04-08 14:37:16 +0200

    chore(deploy-env): check that master deploys with staging env

--- src/cfg/index.ts
+++ src/cfg/index.ts
@@ -3,6 +3,6 @@ import PREVIEW from './preview'
 
 const LIST = process.env.NODE_ENV === 'production' ? PRODUCTION : PREVIEW
 
-console.log('aaaaaaaaaaaaaa', process.env.NODE_ENV)
+console.log('aaaaaaaaaaaaaaa', process.env.NODE_ENV)
 
 export default LIST
