7466a574e3924f646e5a5024d665c202fac9f411
tommasogangemi <tommaso.gangemi@kampaay.com>
2023-04-30 12:07:20 +0200

    feat(token): added handler for token from the server

--- src/api/appendToSpreadsheet.ts
+++ src/api/appendToSpreadsheet.ts
@@ -1,10 +1,13 @@
 import { CFG } from '@/cfg'
+import { getToken } from './authToken'
 
 export const appendToSpreadSheet = async (
   spreadsheetId: string,
   sheetName: string,
   values: string[][]
 ) => {
+  // const { access_token } = await getToken()
+
   return fetch(
     `${CFG.baseUrl}/${spreadsheetId}/values/${sheetName}:append?key=${CFG.googleApiKey}`,
     {
