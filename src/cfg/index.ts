60d1e8376bfb8ef3a4533547b809f5a9b1e9a7cc
tommasogangemi <tommaso.gangemi@kampaay.com>
2023-05-01 10:42:32 +0200

    chore(server-url): changed after server is working!!

--- src/cfg/index.ts
+++ src/cfg/index.ts
@@ -4,7 +4,7 @@ import { spreadSheetId } from './spreadsheets'
 const GOOGLE_API_KEY = 'AIzaSyDW-uIMe2u7DG9ey0GeY2UKncSOnyoLhRU'
 const BASE_URL = 'https://sheets.googleapis.com/v4/spreadsheets'
 const GOOGLE_CLIENT_ID = '807456610735-pl3avc541l77ig87cgcjdd5lknoclts0.apps.googleusercontent.com'
-const SERVER_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : ''
+const SERVER_URL = 'https://cate-e-bombi-server.vercel.app/token'
 
 console.log('NODE ENV', process.env.NODE_ENV)
 
